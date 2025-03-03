9486ae7d85408edda0c35d853e8adfacf5b0b2a9
Alexandre Santos <alexandre.santos@outlook.com>
2025-03-03 13:40:29 -0800

    add SubFeatureB type

--- feature-b/sub-feature-b/SubFeatureB.swift
+++ feature-b/sub-feature-b/SubFeatureB.swift
@@ -1,1 +1,13 @@
-// This is a placeholder for a file under another folder
+// SubFeatureB.swift
+// Alexandre Santos @ 2025
+
+protocol SubFeatureProtocol {
+    var featureId: String { get }
+    var featureName: String { get }
+}
+
+/// Sub Feature B: This is a sub feature of Feature B.
+struct SubFeatureB: SubFeatureProtocol {
+    let featureId: String = "sub-feature-b"
+    let featureName: String = "Sub Feature B"
+}
